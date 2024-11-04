ec1553edeba62306977af6ad47192106e4f1f814
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2024-11-04 02:51:10 +0000

    parte2

--- exemplo/src/test/java/br/pucrs/breno/NextDateTest.java
+++ exemplo/src/test/java/br/pucrs/breno/NextDateTest.java
@@ -2,15 +2,16 @@ package br.pucrs.breno;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple NextDate.
  */
 class NextDateTest {
-    /**
-     * Rigorous Test.
-     */
     @Test
     public void testCalculaProximaData_DataValida() {
         String resultado = NextDate.calculaProximaData("10/08/2004");
@@ -36,8 +37,14 @@ class NextDateTest {
     }
 
     @Test
-    public void testCalculaProximaData_DataForaDoIntervalo() {
+    public void testCalculaProximaData_DataForaDoIntervaloAcima() {
         String resultado = NextDate.calculaProximaData("31/12/9999");
+        assertEquals("ERRO: data invalida.", resultado);
+    }
+
+    @Test
+    public void testCalculaProximaData_DataForaDoIntervalo() {
+        String resultado = NextDate.calculaProximaData("10/08/1000");
         assertEquals("ERRO: data invalida.", resultado);
     }
 
@@ -46,5 +53,38 @@ class NextDateTest {
         String resultado = NextDate.calculaProximaData("32/01/2004");
         assertEquals("ERRO: data invalida.", resultado);
     }
-}
 
+    @Test
+    public void testCalculaProximaData_DataVazia() {
+        String resultado = NextDate.calculaProximaData("");
+        assertEquals("ERRO: tamanho invalido.", resultado);
+    }
+
+    @Test
+    public void testCalculaProximaData_DataComCaracteresInvalidos() {
+        String resultado = NextDate.calculaProximaData("10/08/20AB");
+        assertEquals("ERRO: nao utilizou digito.", resultado);
+    }
+
+    @Test
+    public void testCalculaProximaData_DataComEspacos() {
+        String resultado = NextDate.calculaProximaData("10 / 08 / 2004");
+        assertEquals("ERRO: tamanho invalido.", resultado);
+    }
+
+    @Test
+    public void testMain() {
+        
+        String input = "10/08/2004\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        
+        NextDate.main(new String[]{});
+
+        String expectedOutput = "Informe a data no formato: dd/mm/aaaa\nData seguinte: 11/08/2004\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
